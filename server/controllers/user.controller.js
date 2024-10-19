f6c16e0f34afa3071238f5b49015e4d018956082
ANKIT KUMAR JHA <ankitjha12589@gmail.com>
2024-10-19 11:46:57 +0530

    Clerk WebHooks added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,2 +1,54 @@
+import { Webhook } from "svix";
+import userModel from "../models/user.model.js";
+
 // http://localhost:4000/api/user/webhooks
-const clerkWebhooks = async (req, res) => {};
+const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-signature": req.headers["svix-signature"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+    });
+
+    const { data, type } = req.body;
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          clerkId: data.id,
+          email: data.email_addresses[0].email.address,
+          photo: data.image_url,
+          firstName: data.first_name,
+          lastName: data.last_name,
+        };
+        await userModel.create(userData);
+        res.JSON({});
+        break;
+      }
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email.address,
+          photo: data.image_url,
+          firstName: data.first_name,
+          lastName: data.last_name,
+        };
+        await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+        res.JSON({});
+        break;
+      }
+      case "user.deleted": {
+        await userModel.findOneAndDelete({ clerkId: data.id });
+        res.JSON({});
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.JSON({ success: false, message: error.message });
+  }
+};
+
+export { clerkWebhooks };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connect from "./configs/mongodb.js";
+import userRouter from "./routes/user.route.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
