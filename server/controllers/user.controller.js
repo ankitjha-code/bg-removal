70156e70a9437604865a6523773dfe5855f3042e
ANKIT KUMAR JHA <ankitjha12589@gmail.com>
2024-10-19 12:03:31 +0530

    edit

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -14,40 +14,53 @@ const clerkWebhooks = async (req, res) => {
     const { data, type } = req.body;
     switch (type) {
       case "user.created": {
+        const email = data?.email_addresses?.[0]?.email?.address;
+        if (!email) {
+          throw new Error("Email address not found in request data");
+        }
+
         const userData = {
           clerkId: data.id,
-          email: data.email_addresses[0].email.address,
+          email,
           photo: data.image_url,
           firstName: data.first_name,
           lastName: data.last_name,
         };
         await userModel.create(userData);
-        res.JSON({});
+        res.json({}); // Fixed to use res.json()
         break;
       }
       case "user.updated": {
+        const email = data?.email_addresses?.[0]?.email?.address;
+        if (!email) {
+          throw new Error("Email address not found in request data");
+        }
+
         const userData = {
-          email: data.email_addresses[0].email.address,
+          email,
           photo: data.image_url,
           firstName: data.first_name,
           lastName: data.last_name,
         };
         await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
-        res.JSON({});
+        res.json({}); // Fixed to use res.json()
         break;
       }
       case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id });
-        res.JSON({});
+        res.json({}); // Fixed to use res.json()
         break;
       }
       default: {
+        res
+          .status(400)
+          .json({ success: false, message: "Invalid webhook event type" });
         break;
       }
     }
   } catch (error) {
     console.error(error.message);
-    res.JSON({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message }); // Send status 500 for errors
   }
 };
 
